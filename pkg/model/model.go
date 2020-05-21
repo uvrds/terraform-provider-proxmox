d6f091fb783589e7bf7b97eea3d9aa92a07bae4a
Евгений Басов <ya.mwork@yandex.ru>
2020-05-21 18:13:01 +0300

    try to use enums

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,8 +1,31 @@
 package model
 
-type VMTypes string
+type VMType interface {
+	name() string
+	ordinal() int
+	values() *[]string
+}
+
+type VMTypes uint
 
 const (
-	LXC VMTypes = "LXC"
-	KVM VMTypes = "KVM"
+	LXC VMTypes = iota
+	KVM
 )
+
+var vmTypesStrings = []string{
+	"LXC",
+	"KVM",
+}
+
+func (t VMTypes) name() string {
+	return vmTypesStrings[t]
+}
+
+func (t VMTypes) ordinal() int {
+	return int(t)
+}
+
+func (t VMTypes) values() *[]string {
+	return &vmTypesStrings
+}
