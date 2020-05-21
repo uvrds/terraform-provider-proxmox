920f49671cced0cbd5dc76c5ca02186bb6d2f3c8
Евгений Басов <ya.mwork@yandex.ru>
2020-05-21 20:01:02 +0300

    create vm types

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,31 +1,8 @@
 package model
 
-type VMType interface {
-	name() string
-	ordinal() int
-	values() *[]string
-}
+import "github.com/terraform-provider-proxmox/pkg/model/vmtype"
 
-type VMTypes uint
-
-const (
-	LXC VMTypes = iota
-	KVM
-)
-
-var vmTypesStrings = []string{
-	"LXC",
-	"KVM",
-}
-
-func (t VMTypes) name() string {
-	return vmTypesStrings[t]
-}
-
-func (t VMTypes) ordinal() int {
-	return int(t)
-}
-
-func (t VMTypes) values() *[]string {
-	return &vmTypesStrings
+type virtualMachine struct {
+	name   string
+	vmType vmtype.Alias
 }
